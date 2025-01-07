67bc4f85ebe0f6565ab3ddced8ebe077735775e0
WestonVincze <westonvincze@gmail.com>
2025-01-06 23:49:20 -0500

    ➕ Add leaflet and placeholder Map component

---
+++ src/components/Map.tsx
@@ -0,0 +1,16 @@
+'use client';
+import { useEffect } from "react";
+import * as L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+export const Map = () => {
+  useEffect(() => {
+    const map = L.map('map').setView([51.25, -0.09], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+  }, []);
+
+  return <div id="map" style={{ height: '100vh', width: '100%' }}></div>;
+}
