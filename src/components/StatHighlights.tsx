9c86e5997e7ccef3d4b6913c76d4db776e0c7bb4
WestonVincze <westonvincze@gmail.com>
2025-01-12 14:10:03 -0500

    💄 Finish styles for StatHighlights

--- src/components/StatHighlights.tsx
+++ src/components/StatHighlights.tsx
@@ -7,11 +7,11 @@ export const StatHighlights = (
   { stats }: { stats: Stat[] }
 ) => {
   return(
-    <div className="flex gap-5 justify-center bg-white max-w-screen-lg rounded-lg m-auto">
+    <div className="flex gap-5 justify-center bg-white max-w-screen-lg rounded-lg m-auto p-8 shadow-md">
       {stats.map((stats, i) => (
-        <div key={i} className="flex flex-col w-1/4 p-2">
-          <h4 className="uppercase">{stats.title}</h4>
-          <p className="text-2xl">{stats.data}</p>
+        <div key={i} className="flex flex-col w-1/4 border-l first:border-l-0 pl-8">
+          <h4 className="text-xs tracking-widest uppercase text-dark-gray">{stats.title}</h4>
+          <p className="text-2xl font-medium text-very-dark-gray">{stats.data}</p>
         </div>
       ))}
     </div>
