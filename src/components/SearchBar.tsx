e600f9a37531faa733939095ddd0344f5209c3c3
WestonVincze <westonvincze@gmail.com>
2025-01-28 15:56:33 -0500

    ✨ Add toast messages to display user feedback

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 
@@ -14,9 +15,16 @@ interface SearchBarProps {
 export const SearchBar = ({ placeHolder = "", initialValue = "" }: SearchBarProps) => {
   const router = useRouter();
   const [ip, setIp] = useState(initialValue);
+  const [error, setError] = useState(false);
 
   const handleSubmit = () => {
-    if (!isValidDomain(ip) && !isValidIPAddress(ip)) return;
+    if (!isValidDomain(ip) && !isValidIPAddress(ip)) {
+      toast.error("Please enter a valid ip address or domain");
+      setError(true);
+      return;
+    }
+
+    setError(false);
 
     if (!ip) {
       router.replace('/');
@@ -33,7 +41,7 @@ export const SearchBar = ({ placeHolder = "", initialValue = "" }: SearchBarProp
       // Split the input into segments
       let segments = c.split('.');
 
-      // Automatically add dots after every 3 digits if there are fewer than 4 segments
+      // Automatically add a decimals while user enters ipv4 ip address
       if (segments.length < 4) {
         segments = segments.map(segment => segment.match(/.{1,3}/g)?.join('.') || segment);
         c = segments.join('.');
@@ -46,14 +54,15 @@ export const SearchBar = ({ placeHolder = "", initialValue = "" }: SearchBarProp
   }
 
   return(
-    <div className="flex justify-center min-w-min w-full max-w-[555] mx-auto">
+    <div className={`flex justify-center min-w-min w-full max-w-[555] mx-auto rounded-xl ${error ? 'outline outline-2 outline-red-500' : ''}`}>
       <input
-        className="w-full shadow-md rounded-l-xl text-very-dark-gray py-4 px-6 focus:outline-none text-18px"
+        className={`w-full shadow-md rounded-l-xl text-very-dark-gray py-4 px-6 focus:outline-none text-18px`}
         type="text"
         value={ip}
         onChange={e => handleChange(e.target.value)}
         onKeyDownCapture={e => e.key === "Enter" && handleSubmit()}
         placeholder={placeHolder}
+        onFocus={() => setError(false)}
       />
       <button
         className="shadow-md py-3 px-6 rounded-r-xl bg-gray-900 focus:outline-none hover:bg-gray-700 transition-all duration-300"
@@ -61,6 +70,7 @@ export const SearchBar = ({ placeHolder = "", initialValue = "" }: SearchBarProp
       >
         <Image src="icon-arrow.svg" alt="search" width={11} height={14} />
       </button>
+      <ToastContainer />
     </div>
   )
 }
